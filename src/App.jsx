9be7ae36d1a19c0a846751f2f0bd9c44590596e3
shaimeur <haimeursaad@gmail.com>
2023-04-04 00:57:24 +0000

    fixing the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
 import data from "./data";
 import Column from "./Column";
 
@@ -14,13 +14,12 @@ const App = () => {
 
   return (
     <DragDropContext >
-    <div>
       {list.columnOrder.map((columnId) => {
         const column = list.columns[columnId];
         const tasks = column.taskIds.map((taskId) => list.tasks[taskId]);
         return <Column key={column.id} column={column} tasks={tasks} />;
       })}
-    </div>
+
     </DragDropContext>
   );
 };
