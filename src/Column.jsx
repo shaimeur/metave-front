93a49818eb3555699777996c68714c5010bbee39
shaimeur <haimeursaad@gmail.com>
2023-04-04 02:52:17 +0000

    adding some styling withe the snapshot parameter that go in the callback function in Draggable and Droppable using isDragginOver with ternary operator in the background color to skyblue to heighlight the Column components and the isDraggin in Task component to heighlight the task

--- src/Column.jsx
+++ src/Column.jsx
@@ -14,6 +14,8 @@ const Title = styled.h3`
 
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.2s ease;
+  background-color : ${props =>(props.isDraggingOver ? 'skyblue' : 'white')}
 `;
 
 const Column = (props) => {
@@ -21,10 +23,11 @@ const Column = (props) => {
     <Container>
       <Title>{props.column.title}</Title>
       <Droppable droppableId={props.column.id}>
-        {(provided) => (
+        {(provided,snapshot) => (
           <TaskList
           {...provided.droppableProps}
            ref={provided.innerRef}
+           isDraggingOver={snapshot.isDraggingOver}
            >
             {props.tasks.map((task, index) => (
               <Task key={task.id} task={task} index={index} />
