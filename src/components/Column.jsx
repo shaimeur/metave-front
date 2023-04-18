7b528253fb271ef4a9b5b964c53b38133bf8bbb8
shaimeur <haimeursaad@gmail.com>
2023-04-18 22:59:35 +0000

    adding 2 more column adding the logic for draging and droping in the two others columns

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,24 +4,28 @@ import { Droppable } from "react-beautiful-dnd";
 import Task from "./Task";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  width:50%;
-  max-width:700px;
+  width: 13.75rem;
+  max-width: 90%;
 `;
 const Title = styled.h3`
   padding: 8px;
 `;
 
 const TaskList = styled.div`
+  flex-grow : 1;
+  min-height : 6.25rem
   padding: 12px 16px;
   transition: background-color 0.2s ease;
-  color:black;
-  font-size:1.7rem;
-  cursor:pointer;
-  user-select:none;
-  background-color : ${props =>(props.isDraggingOver ? 'skyblue' : 'white')}
+  color: black;
+  font-size: 1.7rem;
+  cursor: pointer;
+  user-select: none;
+  background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
 `;
 
 const Column = (props) => {
@@ -29,12 +33,12 @@ const Column = (props) => {
     <Container>
       <Title>{props.column.title}</Title>
       <Droppable droppableId={props.column.id}>
-        {(provided,snapshot) => (
+        {(provided, snapshot) => (
           <TaskList
-          {...provided.droppableProps}
-           ref={provided.innerRef}
-           isDraggingOver={snapshot.isDraggingOver}
-           >
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
             {props.tasks.map((task, index) => (
               <Task key={task.id} task={task} index={index} />
             ))}
--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -2,59 +2,92 @@ import React, { useEffect, useState } from "react";
 import { DragDropContext } from "react-beautiful-dnd";
 import data from "../data";
 import Column from "./Column";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const Kanban = () => {
   const [list, setList] = useState(data);
 
-
-
   const dragEndHandler = (result) => {
     // TODO : reorder our column
     const { destination, source, draggableId } = result;
-    if (!destination) {
-      return;
-    }
+
     if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
+      !destination ||
+      (destination.droppableId === source.droppableId &&
+        destination.index === source.index)
     ) {
       return;
     }
 
-    const column = list.columns[source.droppableId];
+    const start = list.columns[source.droppableId];
+    const finish = list.columns[destination.droppableId];
 
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    if (start === finish) {
+      // const newTaskIds = column.taskIds.slice();
+      const newTaskIds = structuredClone(start.taskIds);
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
+      newTaskIds.splice(source.index, 1);
+
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      setList({
+        ...list,
+        columns: {
+          ...list.columns,
+          [start.id]: {
+            ...start,
+            taskIds: newTaskIds,
+          },
+        },
+      });
+      return
+    }
+    // moving from one list to another
+    const startTaskIds = structuredClone(start.taskIds);
+
+    startTaskIds.splice(source.index, 1);
+
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
     };
+
+    const finishTaskIds = structuredClone(finish.taskIds);
+
+    finishTaskIds.splice(destination.index, 0, draggableId);
+
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
     const newState = {
       ...list,
-      columns: {
+      columns : {
         ...list.columns,
-        [newColumn.id]: newColumn,
+        [newStart.id] : newStart,
+        [newFinish.id] : newFinish,
       },
     };
-    setList(newState);
+
+    setList(newState)
   };
 
-
   return (
-
-       <DragDropContext onDragEnd={dragEndHandler}>
-
-
-          {list.columnOrder.map((columnId) => {
-            const column = list.columns[columnId];
-            const tasks = column.taskIds.map((taskId) => list.tasks[taskId]);
-            return <Column key={column.id} column={column} tasks={tasks} />;
-          })}
-
-      </DragDropContext>
-
+    <DragDropContext onDragEnd={dragEndHandler}>
+      <></>
+      <Container>
+        {list.columnOrder.map((columnId) => {
+          const column = list.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => list.tasks[taskId]);
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </Container>
+    </DragDropContext>
   );
 };
 
